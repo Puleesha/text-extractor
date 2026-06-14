01822293429028f5368fcb85a18bfd36956e19c5
Puleesha <puleeshak@gmail.com>
2026-06-14 13:24:12 +0530

    Add Camelot for table extraction

--- SETUP.md
+++ SETUP.md
@@ -30,5 +30,22 @@ pip install marker-pdf
 marker_single odel_report.pdf
 ```
 
+## For camelot: 
+
+1. Create the virtual environment with Python 3.10
+
+```bash
+brew install python@3.10
+/opt/homebrew/bin/python3.10 -m venv .venv
+source .venv/bin/activate
+```
+
+2. Install camelot
+
+```bash
+pip install camelot-py
+pip install tabulate
+```
+
 
 
---
+++ script.py
@@ -0,0 +1,28 @@
+import pymupdf
+# import pymupdf.layout
+# import pymupdf4llm
+
+import camelot
+import os, sys, time
+
+# Pymupdf
+def pumupdfFunc():
+    doc = pymupdf.open("odel_report.pdf")
+    pageOne = doc[1]
+
+    # allTables = pageOne.find_tables()
+    # table1 = allTables.tables[0]
+
+    print(pageOne.get_text())
+
+# Camelot
+def camelotFunc():
+    pdf_path = "odel_report.pdf"
+
+    with pymupdf.open(pdf_path) as document:
+        pageCount = document.page_count
+
+    tables = camelot.read_pdf(pdf_path, pages=f"1 - {pageCount}", flavor="stream")
+    print(tables[3].to_csv("odel.csv"))
+
+camelotFunc()
